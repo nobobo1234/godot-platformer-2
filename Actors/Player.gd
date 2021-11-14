6fbd12557035f52c04793184354d3666cd15f680
Noah van Boven <noahvanboven@gmail.com>
2021-11-14 15:50:00 +0100

    Adding platformer game

---
+++ Actors/Player.gd
@@ -0,0 +1,28 @@
+extends Actor
+
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var direction: = get_direction()
+	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
+	)
+
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	var velocity: = linear_velocity
+	velocity.x = speed.x * direction.x
+	if direction.y != 0.0:
+		velocity.y = speed.y * direction.y
+	if is_jump_interrupted:
+		velocity.y = 0.0
+	return velocity
